2e33976fa3ae6c8c19e4f9b64df4ae725d85e8eb
EvilConundrum <167533567+EvilConundrum@users.noreply.github.com>
2024-05-26 11:23:54 +0800

    docs: add documentation to all of the stack functions

--- .github/workflows/datastructures/stack.c
+++ .github/workflows/datastructures/stack.c
@@ -1,17 +1,36 @@
 #include "stack.h"
 
+/**
+ * Initializes the stack by setting the top index to -1, indicating that the stack is empty.
+ * 
+ * @param stack A pointer to the stack to be initialized.
+ */
 void initStack(Stack* stack) {
     stack->topIndex = -1;
 }
 
+/**
+ * Pushes a new string onto the stack. If the stack is full, prints an error message.
+ * 
+ * @param stack A pointer to the stack where the string will be pushed.
+ * @param string The string to be pushed onto the stack.
+ */
 void push(Stack* stack, char* string) {
     if (!isStackFull(*stack)) {
-        stack->strings[++stack->topIndex] = string;
+        stack->topIndex++;
+        stack->strings[stack->topIndex] = string;
     } else {
         printf("Stack overflow!\n");
     }
 }
 
+/**
+ * Pops the top string off the stack and returns it. If the stack is empty, prints an error message and returns NULL.
+ * 
+ * @param stack A pointer to the stack from which the string will be popped.
+ * 
+ * @return The string at the top of the stack, or NULL if the stack is empty.
+ */
 char* pop(Stack* stack) {
     if (!isStackEmpty(*stack)) {
         return stack->strings[stack->topIndex--];
@@ -21,14 +40,35 @@ char* pop(Stack* stack) {
     }
 }
 
-char* peek(Stack* stack) {
-    return stack->strings[stack->topIndex];
+/**
+ * Returns the string at the top of the stack without removing it.
+ * 
+ * @param stack The stack to peek at.
+ * 
+ * @return The string at the top of the stack.
+ */
+char* peekStack(Stack stack) {
+    return stack.strings[stack.topIndex];
 }
 
+/**
+ * Checks if the stack is empty.
+ * 
+ * @param stack The stack to be checked.
+ * 
+ * @return True if the stack is empty, false otherwise.
+ */
 bool isStackEmpty(Stack stack) {
     return stack.topIndex == -1;
 }
 
+/**
+ * Checks if the stack is full.
+ * 
+ * @param stack The stack to be checked.
+ * 
+ * @return True if the stack is full, false otherwise.
+ */
 bool isStackFull(Stack stack) {
     return stack.topIndex == MAX_SIZE - 1;
 }
