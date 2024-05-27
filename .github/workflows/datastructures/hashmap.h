c75b788d3fcebcabab96583fbb6c4e6db1113f2e
EvilConundrum <167533567+EvilConundrum@users.noreply.github.com>
2024-05-27 08:47:51 +0800

    feat: updated header file to reflect functions implemented in hashmap.c

--- .github/workflows/datastructures/hashmap.h
+++ .github/workflows/datastructures/hashmap.h
@@ -1,1 +1,19 @@
+#include "../calculator.h"
+#include "../utils.c"
 
+#define HASHMAP_SIZE 10
+
+typedef struct KeyValuePairTag {
+    char* key;
+    int value;
+} KeyValuePair;
+
+typedef struct HashMapTag {
+    KeyValuePair* array[HASHMAP_SIZE];
+} HashMap;
+
+unsigned int hash(char* key);
+void initHashMap(HashMap* map);
+void put(HashMap* map, char* key, int value);
+int get(HashMap* map, char* key);
+void freeHashMap(HashMap* map);
